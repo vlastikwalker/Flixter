62a4506af62d51ca3afc45cd8961aac1ba1cc1ce
vlastik <vlastikczech1@gmail.com>
2018-02-27 15:06:57 -0700

    Added lessons underneath sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
-
   namespace :instructor do
-      resources :courses, only: [:new, :create, :show] do
-        resources :sections, only: [:new, :create]
-      end
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
 end
