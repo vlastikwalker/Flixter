a4842aa657447498291889a36ecbdffe9fc087d0
vlastik <vlastikczech1@gmail.com>
2018-03-07 15:24:05 -0700

    Added authentication for enrolled students

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,5 @@
 class LessonsController < ApplicationController
+  before_action  :authenticate_enrollment
   def show
   end
 
@@ -8,4 +9,13 @@ class LessonsController < ApplicationController
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
   end
+
+  def authenticate_enrollment
+    if current_user && current_user.enrolled_in?(current_lesson.section.course)
+    else
+      redirect_to course_path(current_lesson.section.course), alert: 'Not enrolled in the course'
+    end
+  end
+
+
 end
