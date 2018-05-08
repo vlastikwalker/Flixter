e2bdf3dfbee7a5bb67b4b865f053559dcae65fd9
vlastik <vlastikczech1@gmail.com>
2018-05-07 17:39:36 -0700

    Fixed a typo in instructor/section_controller

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_section, only: [:update]
 
   def create
