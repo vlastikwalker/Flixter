fb853218e3f8fb08ea6ba1cf367055ed770453f0
vlastik <vlastikczech1@gmail.com>
2018-08-29 17:57:13 -0700

    changed application.js ordering

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
-//= require popper
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require rails-ujs
+//= require turbolinks
+//= require popper
 //= require bootstrap-sprockets
 //= require_tree .
 
